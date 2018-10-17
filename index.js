240abc80b4063f4546b973e2d0caa65a70b32fdf
Daniel Ruiz <daniel.ruiz@taximo.co>
2018-10-16 23:08:23 -0500

    Creando proyecto

---
+++ index.js
@@ -0,0 +1,40 @@
+(()=>{
+
+    require('rootpath');
+    let csv = require("fast-csv");
+    let fs = require('fs');
+    let path = require('path');
+
+    let fileInput = path.join('.', 'nodo.csv');
+    let fileOutput = path.join('.', 'dataCSV.json');
+
+    let stream = fs.createReadStream(fileInput);
+    let filecontent = [];
+    let csvStream = csv()
+        .on("data", function(data){
+            filecontent.push(data[0].split('|'));
+        })
+        .on("end", function(){
+            let header = filecontent.filter((value,key)=>key == 0)[0]; 
+            let body = filecontent.filter((value, key) => key > 0);
+            let contentData = [];
+
+            for (let i in body){
+                let row = body[i];
+                let newRow = {};
+                for(let o in header){
+                    let column = (header[o] || `lavel_${o}`).replace('\'', '');
+                    newRow[column] = (row[o] || '').replace('\'', '');
+                }
+                contentData.push(newRow);
+            }
+            fs.writeFile(fileOutput, JSON.stringify(contentData), function(error){
+                if (!error){
+                    console.log('success');
+                }
+            });
+        });
+
+    stream.pipe(csvStream);
+
+})();
